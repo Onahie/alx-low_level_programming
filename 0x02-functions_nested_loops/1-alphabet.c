22d0d0686d2c7ea1667e1b6a08601b0e0e3ca04b
Onahie <onahieunice@gmail.com>
2022-12-15 09:12:06 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,11 @@
 
 /**
  * print_alphabet - Entry point
- * Description: a function that prints the alphabets,
+ * Description: a function that prints the alphabet,
  * in lowercase followed by a new line
  * Return: void
  */
+void print_alphabet(void)
 {
 	char letter = 'a';
 	while (letter <= 'z');
