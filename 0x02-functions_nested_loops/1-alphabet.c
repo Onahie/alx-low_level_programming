9755956a22e69f6c2a709702682cdc42840659a6
Onahie <onahieunice@gmail.com>
2022-12-15 11:55:20 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
  * in lowercase followed by a new line
  * Return: void
  */
+
 void print_alphabet(void)
 {
 	char letter = 'a';
 
-	while letter <= 'z';
+	while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
