b40938ac29c09b2dfbb5b170a3ffe24b8f61c7ae
Onahie <onahieunice@gmail.com>
2022-12-20 17:14:53 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the input is upper
+ * @c: input alphabet
+ *
+ * Return: 1(upper), 0(otherwise)
+ */
+int _isupper(int c)
+{
+	int i;
+
+	if ((c >= 'A') && (c <= 'Z'))
+		i = 1;
+	else
+		1 = 0;
+	return (i);
+}
